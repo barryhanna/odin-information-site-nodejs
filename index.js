d99dc3f731d7b65f1e2209f1d6fab9735d0cdd15
Barry Hanna <barryahanna@gmail.com>
2023-05-05 12:54:09 +0100

    Add basic http code

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Works!');
+var http = require('http');
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    console.log(
+      `Connected at: ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+    );
+    res.end(
+      `Hello ${new Date().getHours()}:${new Date().getMinutes()}`
+    );
+  })
+  .listen(8080);
